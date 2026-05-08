b28e1179020e1b858e68672dd5fccd03e11281f3
DKDM <djibkdm@gmail.com>
2026-05-08 08:49:31 +0100

    Refactor HTML structure and update JavaScript to display card values

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardEl = document.querySelector('#card-el')
 
 function startGame() {
     sumEl.textContent = 'Somme: ' + sum
+    cardEl.textContent = 'Cartes:' + firstCard + ' ' + secondCard
 
     if (sum <= 20) {
         message = "Voulez-vous tirer une nouvelle carte ?"
@@ -21,8 +23,17 @@ function startGame() {
 
     }
     messageEl.textContent = message
-
-
 }
 
 
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+    // 1. Create a card variable, and hard code its value to a number (2-11)
+    let card = 5
+    // 2. Add the new card to the sum variable
+    
+    
+    
+    // 3. Call startGame()
+
+}
