b4aa9b663884b7c8e01a54b57bf35683d0d4937d
DKDM <djibkdm@gmail.com>
2026-05-12 15:27:15 +0100

    update project

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-let firstCard = 10
-let secondCard = 14
-let sum = firstCard + secondCard
+
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
+let cards = []
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardEl = document.querySelector('#card-el')
 
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 1) {
+        return 1
+    } else if (randomNumber > 10) {
+        return 10
+    }
+    return randomNumber
+}
+
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = 'Somme: ' + sum
-    cardEl.textContent = 'Cartes:' + firstCard + ' ' + secondCard
+    cardEl.textContent = "Cartes: "
+    for (let i = 0; i < cards.length; i++) {
+        cardEl.textContent = "Cartes :" + cards + " "
+    }
 
     if (sum <= 20) {
         message = "Voulez-vous tirer une nouvelle carte ?"
@@ -20,20 +43,16 @@ function startGame() {
     } else {
         message = "Vous avez perdu !"
         isAlive = false
-
     }
     messageEl.textContent = message
 }
 
-
 function newCard() {
-    console.log("Drawing a new card from the deck!")
-    // 1. Create a card variable, and hard code its value to a number (2-11)
-    let card = 5
-    // 2. Add the new card to the sum variable
-    
-    
-    
-    // 3. Call startGame()
+
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    startGame()
+    console.log(cards)
 
 }
